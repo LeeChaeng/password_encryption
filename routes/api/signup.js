3073073a184e8c2496ffa9c1a68ed53dc3c5a520
Luna Lee <htijl@naver.com>
2021-10-31 18:48:34 +0900

    write signup logic

--- routes/api/signup.js
+++ routes/api/signup.js
@@ -1,5 +1,6 @@
 import express from "express";
 import db from "../../data/db";
+import bctypt from "bcrypt";
 
 const router = express.Router();
 
@@ -7,11 +8,23 @@ router.post("/", function (req, res, next) {
   const id = req.body.id;
   const pw = req.body.pw;
 
-  db.get("users").push({ id: id, pw: pw }).write();
+  const saltRounds = 10;
 
-  // todo encryption
+  bctypt
+    .genSalt(saltRounds)
+    .then((salt) => {
+      console.log(`Salt: ${salt}}`);
+      return bctypt.hash(pw, salt);
+    })
+    .then((hash) => {
+      console.log(`Hash: ${hash}`);
 
-  res.send("signup completed!");
+      db.get("users").push({ id: id, digest: hash }).write();
+      res.render("success", { title: "회원가입" });
+    })
+    .catch((err) => {
+      res.send("Something went wrong");
+    });
 });
 
 module.exports = router;
