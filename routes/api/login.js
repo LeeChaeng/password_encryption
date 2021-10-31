67d5454cb80e1732efe681bdfaa4e1264bff5825
Luna Lee <htijl@naver.com>
2021-10-31 20:05:31 +0900

    write login logic

--- routes/api/login.js
+++ routes/api/login.js
@@ -1,10 +1,24 @@
 import express from "express";
+import db from "../../data/db";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
 /* GET users listing. */
 router.post("/", function (req, res, next) {
-  res.send("login");
+  const id = req.body.id;
+  const pw = req.body.pw;
+
+  const digest = db.get("users").find({ id: id }).value().digest ?? "";
+
+  bcrypt
+    .compare(pw, digest)
+    .then((result) =>
+      result
+        ? res.render("success", { title: "로그인" })
+        : res.send("로그인 정보가 맞지 않습니다.")
+    )
+    .catch((err) => res.send("Something went wrong"));
 });
 
 module.exports = router;
